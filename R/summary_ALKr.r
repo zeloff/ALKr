c95fafe656eac7d1895ed8c86c6e1eb3b5930a75
Zé Loff <jfloff@phistat.com>
2013-01-30 12:33:18 +0000

    Fix printing parameters on 'show' methods

--- R/summary_ALKr.r
+++ R/summary_ALKr.r
@@ -12,7 +12,7 @@ setMethod("show",
     function(object) {
       print(cbind(pj = object@pj, mean_lj = object@mean_lj, var_lj = object@var_lj))
       cat(paste("\nMethod:", object@method,"\n"))
-      if(length(object@parameters) > 1)
+      if(length(object@parameters) > 0)
         print(matrix(object@parameters, dimnames = list(names(object@parameters), "Value")))
     }
 )
