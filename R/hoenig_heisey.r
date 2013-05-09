983ef60929f63c95d53f619d9877ae8c405a296d
Ze Loff <jfloff@phistat.com>
2013-05-09 12:07:03 +0100

    Fix bug in hoenig_heisey.R

--- R/hoenig_heisey.r
+++ R/hoenig_heisey.r
@@ -6,7 +6,7 @@ hoenig_heisey <- function(x, fi1, fi2, threshold = 0.0001, maxiter = 2000,
                           description = "") {
   
   nij1 <- fi1 * calc_ALK(x)
-  nij2 <- fi2 * calc_invALK(x, fi1)
+  nij2 <- fi2 * calc_ALK(x)
   pj2 <- colSums(nij2) / sum(nij2)
   
   criterion <- 1
@@ -15,12 +15,14 @@ hoenig_heisey <- function(x, fi1, fi2, threshold = 0.0001, maxiter = 2000,
   while(criterion > threshold & iter < maxiter) {
     pj2.old <- pj2
     ialk <- sweep(nij1 + nij2, 2, colSums(nij1 + nij2), "/")
-    pj2 <- colSums(nij2) / sum(nij2)
+    ialk[is.nan(ialk)] <- 0
     numer <- sweep(ialk, 2, colSums(nij2), "*")
     denom <- rowSums(numer)
     denom[denom == 0] <- 1
     nij2 <- fi2 * sweep(numer, 1, denom, "/")
+    pj2 <- colSums(nij2) / sum(nij2)
     criterion <- sum(abs(pj2 - pj2.old))
+    iter <- iter + 1
   }
   
   new("ALKr", alk = calc_ALK(nij2),
